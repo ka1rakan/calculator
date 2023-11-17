8e447ce40d7ff88cb1b48659557ca1d6397558fc
ka1rakan <hsuhankaraca@gmail.com>
2023-11-17 17:03:09 +0300

    add operation and being able to do multiple operations in a row

--- script.js
+++ script.js
@@ -1,16 +1,18 @@
 const numbers = document.querySelectorAll(".number");
-const operators = document.querySelectorAll(".operator")
-const displayP = document.querySelector(".display p")
+const operators = document.querySelectorAll(".operator");
+const specials = document.querySelectorAll(".special");
+const displayP = document.querySelector(".display p");
 
 let a=0;
 let b=0;
 let operator="";
+
 operators.forEach((op)=>{
     op.addEventListener("click",(e)=>{
-        operator += e.target.innerText;
-        console.log(operator)
+        operator = e.target.innerText;
     })
 })
+
 numbers.forEach((number)=>{
     number.addEventListener("click",(e)=>{
         if(operator){
@@ -18,6 +20,35 @@ numbers.forEach((number)=>{
         }else{
             a+= e.target.innerText
         }
-        console.log(a,b)
+    })
+})
+
+let result;
+function operation(a,b,operator){
+    a = Number(a);
+    b = Number(b);
+    if(operator=="%"){
+       result = a%b 
+    }else if(operator=="/"){
+        result = a/b
+    }else if(operator=="x"){
+        result = a*b
+    }else if(operator=="-"){
+        result = a-b
+    }else if(operator=="+"){
+        result = a+b
+    }
+}
+
+specials.forEach((special)=>{
+    special.addEventListener("click",(e)=>{
+        if(e.target.innerText=="="){
+            console.log(a,b,operator)
+            operation(a,b,operator)
+            console.log(result)
+            a=0;
+            b=0;
+            operator="";
+        }
     })
 })
