34bcce69663f8d908cf81e24f56fae35e56f9b04
ka1rakan <hsuhankaraca@gmail.com>
2023-11-17 16:34:34 +0300

    add event listeners to numbers and operators and add some logic

--- script.js
+++ script.js
@@ -1,8 +1,23 @@
-const buttons = document.querySelectorAll(".button");
+const numbers = document.querySelectorAll(".number");
+const operators = document.querySelectorAll(".operator")
 const displayP = document.querySelector(".display p")
 
-buttons.forEach((button)=>{
-    button.addEventListener("click",(e)=>{
-        displayP.innerText = e.target.innerText;
+let a=0;
+let b=0;
+let operator="";
+operators.forEach((op)=>{
+    op.addEventListener("click",(e)=>{
+        operator += e.target.innerText;
+        console.log(operator)
+    })
+})
+numbers.forEach((number)=>{
+    number.addEventListener("click",(e)=>{
+        if(operator){
+            b+= e.target.innerText;
+        }else{
+            a+= e.target.innerText
+        }
+        console.log(a,b)
     })
 })
